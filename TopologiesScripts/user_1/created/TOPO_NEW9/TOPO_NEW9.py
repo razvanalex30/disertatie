936ddcee687f050f0216dd2f207cc9236e1b0f1a
razvanalex30 <razvan_alex97@yahoo.com>
2023-08-24 10:39:06 +0300

    fixed small typo

--- TopologiesScripts/user_1/created/TOPO_NEW9/TOPO_NEW9.py
+++ TopologiesScripts/user_1/created/TOPO_NEW9/TOPO_NEW9.py
@@ -18,20 +18,26 @@ def myNetwork():
                    build=False,
                    waitConnected=True)
 
-    info( '*** Adding controllers\n' )
-    # Insert Controllers here
+    info( '*** Adding controller\n' )
+    c1 = net.addController( name='c1', controller=Controller, protocol='tcp', port=1234 )
+    c2 = net.addController( name='c2', controller=Controller, protocol='tcp', port=555 )
 
     info( '*** Add switches\n')
-    # Insert Switches here
+    s1 = net.addSwitch('s1', cls=OVSKernelSwitch)
+    s2 = net.addSwitch('s2', cls=OVSKernelSwitch)
 
     info('*** Add routers\n')
     # Insert Routers here
 
     info( '*** Add hosts\n')
-    # Insert Hosts here
+    h1 = net.addHost('h1', cls=Host, ip = '192.168.0.1/24', mac = '00:11:22:33:44:55', defaultRoute='via 192.168.0.2')
+    h2 = net.addHost('h2', cls=Host, ip = '192.168.1.1/24', defaultRoute='via 192.168.0.3')
+    h3 = net.addHost('h3', cls=Host, ip = '10.0.0.4/16')
 
     info( '*** Add links\n')
-    # Insert links here
+    net.addLink(h1, s1)
+    net.addLink(h2, s1)
+    net.addLink(h3, s2)
 
     info( '*** Starting network\n')
     net.build()
@@ -40,7 +46,8 @@ def myNetwork():
         controller.start()
 
     info( '*** Starting switches\n')
-    # Insert switches controllers links here
+    net.get('s1').start([c1])
+    net.get('s2').start([c2])
 
     info( '*** Post configure switches and hosts\n')
     # Insert router config here
@@ -56,6 +63,6 @@ def myNetwork():
 
 if __name__ == '__main__':
     setLogLevel( 'info' )
-    logfile = "#LOGFILE"
+    logfile = "/home/razvan/Disertatie/disertatie/TopologiesScripts/user_1/created/TOPO_NEW9/logfile.log"
     logging.basicConfig(format='%(message)s', filename=logfile, filemode='a', level=logging.INFO)
     myNetwork()
